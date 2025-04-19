4e093d606a81d81e1f05a5ea1ad39dff3abf47f3
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-19 23:12:29 +0000

    Volume of Sphere

--- results.php
+++ results.php
@@ -1,10 +1,11 @@
 <?php
 	// get the base and height from the textfields
 	$radius = $_POST['radius'];
-	const PI = Math.PI
+	const PI = Math.PI;
 
 	// calculate the area
-	$volume = (4/3) * PI * $radius**3
+	$volume = (4/3) * PI * pow($radius,3);
+	$volume = round($volume,2);
 ?>
 	<h3>Results:</h3>
-	The volume of the sphere is <?php echo "$volume" ?>cm<sup>2</sup>.
+	The volume of the sphere is <?php echo "$volume" ?>cm<sup>3</sup>.
