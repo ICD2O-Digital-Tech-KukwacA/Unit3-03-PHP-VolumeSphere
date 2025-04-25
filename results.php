2271e275a871c813c72258b90b434d28e4777369
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-25 15:18:33 +0000

    Update 2 Unit3-03

--- results.php
+++ results.php
@@ -1,11 +1,11 @@
 <?php
 	// get the radius from the textfields
-	$radius = $_POST['base1'];
+	$radius = $_POST['radius'];
 
 	// calculate the volume of the sphere
-	const $PI = 3.14;
+	$PI = 3.14;
     $volume = (4/3) * $PI * pow($radius,3);
-    $volume = round($volume, 2);
+    $volume = round($volume,2);
 ?>
 	<h3>Results:</h3>
 	The volume of the Sphere is <?php echo "$volume" ?>cm<sup>3</sup>.
